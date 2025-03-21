537afc640f0ab77beca1c10c7abc1bef7a5a740e
Arian van Putten <arian.vanputten@gmail.com>
2025-03-21 15:24:27 +0000

    use microkernel

---
+++ packages/kernel/package.nix
@@ -0,0 +1,15 @@
+{
+  stdenv,
+  linuxPackages_custom,
+  fetchgit,
+}:
+(linuxPackages_custom rec {
+  version = "6.1.128-3.201.amzn2023";
+  modDirVersion = "6.1.128";
+  src = fetchgit {
+    url = "https://github.com/amazonlinux/linux";
+    tag = "microvm-kernel-6.1.128-3.201.amzn2023";
+    hash = "sha256-HZ3wyrg+tcMrw4jjCmHBwmwhVrh/NkuSEBxWg/fIPyk=";
+  };
+  configfile = ./microvm-kernel-ci-aarch64-6.1.config;
+}).kernel
