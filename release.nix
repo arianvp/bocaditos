5fd6ce15eeb0fd65a6cc25201d8d73dcc56beb9c
Arian van Putten <arian.vanputten@gmail.com>
2025-04-14 17:38:11 +0200

    arm builds

--- release.nix
+++ release.nix
@@ -1,5 +1,17 @@
 let
   sonos = import ./.;
+  inherit (import ./npins) nixpkgs;
+  lib = import "${nixpkgs}/lib";
 in
-  # TODO: re-export passthru.tests
-  sonos 
+# TODO: re-export passthru.tests
+lib.recurseIntoAttrs (
+  lib.genAttrs [ "aarch64-linux" "x86_64-linux" ] (
+    system:
+    lib.pipe { inherit system; } [
+      sonos
+      (lib.filterAttrs (_: lib.isDerivation))
+      (lib.mapAttrs (_: lib.hydraJob))
+      lib.recurseIntoAttrs
+    ]
+  )
+)
