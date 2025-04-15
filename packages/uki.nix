63e0a734aaf21487d9424ba651f057048fe37e32
Arian van Putten <arian.vanputten@gmail.com>
2025-04-15 10:59:31 +0000

    Work around https://github.com/systemd/systemd/issues/37133
    We can not specify phases to measure in ukify without also providing
    a pcr-private-key
    
    This is an upstream bug.
    
    We work around it currently by using a fake key and then
    scrubbing it again

--- packages/uki.nix
+++ packages/uki.nix
@@ -2,6 +2,7 @@
   systemd,
   kernel,
   openssl,
+  binutils,
   initrd,
   cmdline ? "",
   os-release,
@@ -12,27 +13,26 @@
   isUnifiedSystemImage ? true,
 }:
 let
+  linux = "${kernel}/${stdenv.hostPlatform.linux-kernel.target}";
+  initrd' = "${initrd}/initrd";
+  uname = kernel.version;
   args = lib.cli.toGNUCommandLineShell { } {
-    linux = "${kernel}/${stdenv.hostPlatform.linux-kernel.target}";
-    initrd = "${initrd}/initrd";
+    inherit linux uname;
+    initrd = initrd';
     # NOTE: providing uname here to avoid ukify from trying to decompress the kernel
-    uname = kernel.version;
     # TODO: This is a hack. This should just work through pkg-config in my opinion
     # but ukify hardcodes /usr/lib/systemd/boot/efi instead of using bootlibdir. Not sure how to fix that
     # TODO: Fix cross-compilation. Normally splicing takes care of that
     stub = "${systemd}/lib/systemd/boot/efi/linux${stdenv.hostPlatform.efiArch}.efi.stub";
+    os-release = "@${os-release}";
+    inherit cmdline;
     # systemd-measure etc
     tools = [
       "${systemd}/lib/systemd"
       "${systemd}/bin"
     ];
-    os-release = "@${os-release}";
-    inherit cmdline;
-    measure = true;
+
     json = "pretty";
-    phases = if isUnifiedSystemImage then "sysinit,sysinit:ready" else null;
-    pcr-private-key = "private-key.pem";
-    pcr-public-key = "public-key.pem";
   };
 in
 let
@@ -42,6 +42,7 @@ let
         nativeBuildInputs = [
           systemd
           openssl
+          binutils
           jq
         ];
         # allowedReferences = [ kernel ];
@@ -49,17 +50,56 @@ let
           nativeBuildInputs = [
             systemd
           ];
-        } "ukify inspect ${uki}/vmlinux.efi --json=pretty | tee $out";
+        } "ukify inspect ${uki}/uki.efi --json=pretty | tee $out";
       }
       ''
         mkdir -p $out
-        # NOTE: signing with a throw-away key for now
-        openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -out private-key.pem
-        openssl rsa -pubout -in private-key.pem -out public-key.pem
+        # TODO: signing with a throw-away key for now. This means any new image invalidates encrypted credentials and luks partitions
+        # NOTE: These are FAKE keys just to convince ukify to run
+        openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -out tpm2-pcr-private-key.pem
+        openssl rsa -pubout -in tpm2-pcr-private-key.pem -out tpm2-pcr-public-key.pem
 
-        ukify build --output "$out/vmlinux.efi" ${args}
-        cp public-key.pem "$out/pcrpkey.pem"
-        ukify inspect "$out/vmlinux.efi" --json=pretty | jq -r '.[".pcrsig"].text' | jq . > "$out/pcrsig.json"
+        # TODO: I'd want the following to work; but it doesn't: https://github.com/systemd/systemd/issues/37133#issuecomment-2802444683
+        # ukify build \
+        #    --output "$out/vmlinux.efi"  \
+        #    --pcr-public-key "tpm2-pcr-public-key.pem" \
+        #    --policy-digest
+        #    --phases "sysinit,sysinit:ready" \
+        #    ${args}
+
+        # cp tpm2-pcr-public-key.pem "$out/tpm2-pcr-public-key.pem"
+        # to emulate; we sign with a throaway key and then remove the signature
+        ls ${systemd}/lib/systemd/systemd-keyutil
+
+        # TODO: For some reason it can not find systemd-keyutil. no idea why. so need to pass pcr-public-key explicitly
+        # FileNotFoundError: [Errno 2] No such file or directory: '/usr/lib/systemd/systemd-keyutil'
+
+        ukify build \
+            --tools "${systemd}/lib/systemd" \
+            --output "uki.efi" \
+            ${args} \
+            --pcr-private-key tpm2-pcr-private-key.pem \
+            --pcr-public-key tpm2-pcr-public-key.pem \
+            --phases "sysinit,sysinit:ready" \
+            --measure \
+            --json=pretty \
+            > "$out/measure.json"
+
+        # HACK:
+        objcopy --remove-section=.pcrsig --remove-section=.pcrpkey "uki.efi" "$out/uki.efi"
+        inspect=$(ukify inspect "$out/uki.efi" --json=short)
+        echo "$inspect" | jq .sbat.text > .sbat
+        echo "$inspect" | jq .osrel.text > .osrel
+        echo "$inspect" | jq .uname.text > .uname
+
+        ${systemd}/lib/systemd/systemd-measure policy-digest \
+            --sbat=.sbat \
+            --osrel=.osrel \
+            --uname=.uname \
+            --linux=${linux} \
+            --initrd=${initrd'} \
+            --json=pretty \
+            --pcrpkey=tpm2-pcr-public-key.pem > "$out/pcrs.json"
       '';
 in
 uki
